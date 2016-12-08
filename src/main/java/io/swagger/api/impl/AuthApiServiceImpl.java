6a582537328a41db73b073864af88349ae12fbd5
David Nellis <dnellis74@gmail.com>
2016-12-07 18:02:31 -0700

    Made the response match the swagger as best as possible.

--- src/main/java/io/swagger/api/impl/AuthApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/AuthApiServiceImpl.java
@@ -1,27 +1,27 @@
 package io.swagger.api.impl;
 
-import io.swagger.api.*;
-import io.swagger.model.*;
-
-import io.swagger.model.IsAvailable;
-
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
-import io.swagger.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.wso2.msf4j.formparam.FormDataParam;
-import org.wso2.msf4j.formparam.FileInfo;
+import java.util.Random;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
+
+import io.swagger.api.ApiResponseMessage;
+import io.swagger.api.AuthApiService;
+import io.swagger.api.NotFoundException;
+import io.swagger.model.IsAvailable;
+
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaMSF4JServerCodegen", date = "2016-12-07T16:49:05.689-07:00")
 public class AuthApiServiceImpl extends AuthApiService {
     @Override
     public Response authCheckUsernameAvailabilityUsernameGet(String username
  ) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	//TODO this really isn't exposed
+    	IsAvailable result = new IsAvailable();
+    	List<Boolean> result2 = new ArrayList<Boolean>();
+    	result2.add( new Boolean(new Random().nextBoolean()));
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, result2.toString())).build();
     }
 }
